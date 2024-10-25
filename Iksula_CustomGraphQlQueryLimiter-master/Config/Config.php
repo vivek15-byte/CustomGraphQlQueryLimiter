691aab002b0be64dfe6b98405683c76fd397f83d
vivek15-byte <vs15021994@gmail.com>
2024-10-25 16:20:07 +0530

    Add files via upload

---
+++ Iksula_CustomGraphQlQueryLimiter-master/Config/Config.php
@@ -0,0 +1,39 @@
+<?php declare(strict_types=1);
+
+namespace Iksula\CustomGraphQlQueryLimiter\Config;
+
+use Magento\Framework\App\Config\ScopeConfigInterface;
+
+class Config
+{
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $scopeConfig;
+
+    /**
+     * Config constructor.
+     * @param ScopeConfigInterface $scopeConfig
+     */
+    public function __construct(
+        ScopeConfigInterface $scopeConfig
+    ) {
+        $this->scopeConfig = $scopeConfig;
+    }
+
+    /**
+     * @return int
+     */
+    public function getQueryDepth(): int
+    {
+        return (int) $this->scopeConfig->getValue('graphql_query_limiter/settings/query_depth');
+    }
+
+    /**
+     * @return int
+     */
+    public function getQueryComplexity(): int
+    {
+        return (int) $this->scopeConfig->getValue('graphql_query_limiter/settings/query_complexity');
+    }
+}
---
+++ Iksula_CustomGraphQlQueryLimiter-master/Plugin/QueryProcessorPlugin.php
@@ -0,0 +1,55 @@
+<?php declare(strict_types=1);
+
+namespace Iksula\CustomGraphQlQueryLimiter\Plugin;
+
+use GraphQL\Error\Error;
+use GraphQL\Language\AST\DocumentNode;
+use GraphQL\Validator\DocumentValidator;
+use GraphQL\Validator\Rules\QueryComplexity;
+use GraphQL\Validator\Rules\QueryDepth;
+use Magento\Framework\GraphQl\Query\QueryProcessor;
+use Magento\Framework\GraphQl\Query\Resolver\ContextInterface;
+use Magento\Framework\GraphQl\Schema;
+use Iksula\CustomGraphQlQueryLimiter\Config\Config;
+
+class QueryProcessorPlugin
+{
+    /**
+     * @var Config
+     */
+    private $config;
+
+    /**
+     * QueryComplexityLimiterPlugin constructor.
+     * @param Config $config
+     */
+    public function __construct(
+        Config $config
+    ) {
+        $this->config = $config;
+    }
+
+    /**
+     * @param QueryProcessor $queryProcessor
+     * @param Schema $schema
+     * @param string $source
+     * @param ContextInterface|null $contextValue
+     * @param array|null $variableValues
+     * @param string|null $operationName
+     * @return array
+     * @throws Error
+     */
+    public function beforeProcess(
+        QueryProcessor $queryProcessor,
+        Schema $schema,
+        DocumentNode|string $source,
+        ContextInterface $contextValue = null,
+        array $variableValues = null,
+        string $operationName = null
+    ) {
+        DocumentValidator::addRule(new QueryComplexity($this->config->getQueryComplexity()));
+        DocumentValidator::addRule(new QueryDepth($this->config->getQueryDepth()));
+
+        return [$schema, $source, $contextValue, $variableValues, $operationName];
+    }
+}
